9b00a93f917363af39b7d624840f62107f7d7dd7
ARASH FAD <fadarash@gmail.com>
2023-03-25 11:01:41 +0330

    test

--- src/pages/justTest.tsx
+++ src/pages/justTest.tsx
@@ -1,48 +1,9 @@
-import { Button, TextField } from "@mui/material";
-import { Box } from "@mui/system";
-import React, { useState } from "react";
-//write a regexp that don't allow to use any special character
-//write a function to validate website url
+import React from 'react'
 
-const regexp = /[^a-zA-Z0-9]/;
-
-const JustTest = () => {
-  const [inputState, setInputState] = useState("");
-  const [error, setError] = useState(false);
-
-  const validation = (value?: string) =>
-    value && regexp.test(value) ? true : false;
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    if (validation(value)) setError(true);
-    setInputState(value);
-    setError(false);
-  };
-
-  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    if (validation(value)) setError(true);
-    console.log("value");
-  };
-
+const justTest = () => {
   return (
-    <Box sx={{ width: "50%", m: "auto" }}>
-      <TextField
-        fullWidth
-        error={error}
-        helperText={error ? "it's not valid" : " "}
-        id="outlined-error-helper-text"
-        label="Error"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        // type="email"
+    <div>justTest</div>
+  )
+}
 
-        value={inputState}
-      />
-      {/* <Button onClick={handleSubmit}>SUBMIT</Button> */}
-    </Box>
-  );
-};
-
-export default JustTest;
+export default justTest
